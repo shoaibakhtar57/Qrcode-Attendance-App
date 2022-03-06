21d27d51262535a1c82dc20ce5897b982d49442a
shoaibakhtar57 <shoaib.dev57@gmail.com>
2022-03-06 20:38:54 +0500

    one signal integration

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -8,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class AuthController extends GetxController {
   final AuthRepo authRepo = AuthRepo();
@@ -143,9 +144,45 @@ class AuthController extends GetxController {
     password.clear();
   }
 
+  Future<void> initPlatformState() async {
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      print("Accepted permission: $accepted");
+    });
+
+    OneSignal.shared.setNotificationWillShowInForegroundHandler(
+        (OSNotificationReceivedEvent event) {
+      // Will be called whenever a notification is received in foreground
+      // Display Notification, pass null param for not displaying the notification
+      event.complete(event.notification);
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // Will be called whenever a notification is opened/button pressed.
+    });
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      // Will be called whenever the permission changes
+      // (ie. user taps Allow on the permission prompt in iOS)
+    });
+
+    OneSignal.shared
+        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      // Will be called whenever the subscription changes
+      // (ie. user gets registered with OneSignal and gets a user ID)
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver(
+        (OSEmailSubscriptionStateChanges emailChanges) {
+      // Will be called whenever then user's email subscription changes
+      // (ie. OneSignal.setEmail(email) is called and the user gets registered
+    });
+  }
+
   @override
   void onInit() {
-    persistanceLogin();
+    initPlatformState();
+    // persistanceLogin();
     super.onInit();
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.verbose);
+  await OneSignal.shared.setAppId('bd3fddef-2c1d-464b-b1a9-f6eaf33e7ae4');
 
   runApp(
     GetMaterialApp(
